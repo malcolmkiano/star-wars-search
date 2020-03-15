e68cce505fa5a9dac74cdbe2ce2f89333272fd33
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-15 11:56:32 -0400

    Updated app colors, increased contrast for visibility

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,6 +6,7 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
+        <div className="links sharethis-inline-share-buttons"></div>
         <p>Designed & developed by <a href="https://malcolmkiano.com" target="_blank" rel="noopener noreferrer">Malcolm Kiano</a>.</p>
       </footer>
     )
