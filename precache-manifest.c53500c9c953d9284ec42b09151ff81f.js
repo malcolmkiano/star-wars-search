c3f2c6def3ed6f69ccee236e12ff35272e09466b
Malcolm Kiano <malcolmkiano@gmail.com>
2020-03-15 23:24:47 -0400

    Updates

---
+++ precache-manifest.c53500c9c953d9284ec42b09151ff81f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effd95c2d22bb1ba57d24ee63f9ec618",
+    "url": "/star-wars-search/index.html"
+  },
+  {
+    "revision": "aa8a26efe956afafd111",
+    "url": "/star-wars-search/static/css/main.a5c6103b.chunk.css"
+  },
+  {
+    "revision": "60c0c7a3c280abd2a2c5",
+    "url": "/star-wars-search/static/js/2.df5ab694.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/star-wars-search/static/js/2.df5ab694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8a26efe956afafd111",
+    "url": "/star-wars-search/static/js/main.c6040573.chunk.js"
+  },
+  {
+    "revision": "8151f781d45233dd1d77",
+    "url": "/star-wars-search/static/js/runtime-main.4ac9aa7e.js"
+  },
+  {
+    "revision": "34ea7c1c6e5db78e7042e96adcc432cc",
+    "url": "/star-wars-search/static/media/starfield.34ea7c1c.jpg"
+  },
+  {
+    "revision": "c41f740a5bf4b1e663ee3184b08677f7",
+    "url": "/star-wars-search/static/media/swlogo.c41f740a.svg"
+  }
+]);
